45d2a3b9ea9f4fa43c68a46a85f75fa661565c26
Hudson Brendon <hudson.brendon.precocerto.co>
2021-06-24 05:25:42 +0000

    Atualiza os atributos do sensor.

--- custom_components/drivvo/sensor.py
+++ custom_components/drivvo/sensor.py
@@ -1,23 +1,38 @@
-"""
-For more details on this component, refer to the documentation at
-https://github.com/hudsonbrendon/sensor.drivvo
-"""
+import hashlib
+import json
 import logging
-from datetime import timedelta
+import string
+from collections import defaultdict
+from datetime import datetime, timedelta
 
-import async_timeout
 import homeassistant.helpers.config_validation as cv
+import pytz
+import requests
 import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.const import (
+    ATTR_ATTRIBUTION,
+    CONF_NAME,
+    CONF_RESOURCES,
+    STATE_UNKNOWN,
+)
 from homeassistant.helpers.entity import Entity
+
+_LOGGER = logging.getLogger(__name__)
+
+ICON = "mdi:gas-station"
+
+SCAN_INTERVAL = timedelta(minutes=60)
+
+ATTRIBUTION = "Data provided by drivvo api"
+
+DOMAIN = "drivvo"
 
 CONF_EMAIL = "email"
 CONF_PASSWORD = "password"
 CONF_MODEL = "model"
 CONF_ID_VEHICLE = "id_vehicle"
 
-SCAN_INTERVAL = timedelta(minutes=60)
 
 LOGIN_BASE_URL = "https://api.drivvo.com/autenticacao/login"
 BASE_URL = "https://api.drivvo.com/veiculo/{}/{}/web"
@@ -31,61 +46,71 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
-_LOGGER = logging.getLogger(__name__)
+
+def get_data(email, password, id_vehicle):
+    """Get The request from the api"""
+    password = hashlib.md5(password.encode("utf-8")).hexdigest()
+    url = BASE_URL.format(id_vehicle, "abastecimento")
+    supplies = []
+
+    response = requests.post(
+        LOGIN_BASE_URL,
+        data=dict(email=email, senha=password),
+    )
+
+    if response.ok:
+        x_token = response.json().get("token")
+        response = requests.get(url, headers={"x-token": x_token})
+        if response.ok:
+            supplies = response.json()
+        else:
+            _LOGGER.error("Cannot perform the request")
+    else:
+        _LOGGER.error("Cannot authentication")
+    return supplies
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Configuração do sensor."""
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Setup the currency sensor"""
+    name = "{} - Abastecimento".format(config["model"])
     email = config["email"]
     password = config["password"]
     model = config["model"]
     id_vehicle = config["id_vehicle"]
-    session = async_create_clientsession(hass)
-    name = "Abastecimento"
-    async_add_entities(
-        [DrivvoSupplySensor(email, password, model, id_vehicle, name, session)], True
+
+    add_entities(
+        [DrivvoSensor(hass, name, email, password, model, id_vehicle, SCAN_INTERVAL)],
+        True,
     )
 
 
-class DrivvoSupplySensor(Entity):
-    """Sensor de abastecimento"""
-
-    def __init__(self, email, password, model, id_vehicle, name, session):
-        self._state = model
-        self._model = model
+class DrivvoSensor(Entity):
+    def __init__(self, hass, name, email, password, model, id_vehicle, interval):
+        """Inizialize sensor"""
+        self._state = STATE_UNKNOWN
+        self._hass = hass
+        self._interval = interval
         self._email = email
         self._password = password
+        self._model = model
         self._id_vehicle = id_vehicle
-        self.session = session
         self._name = name
         self._supplies = []
 
-    async def async_update(self):
-        """Atualização do sensor."""
-        _LOGGER.debug("%s - Running update", self._name)
-        try:
-            url = BASE_URL.format(self._id_vehicle, "abastecimento")
-            async with async_timeout.timeout(10, loop=self.hass.loop):
-                response = await self.session.post(
-                    LOGIN_BASE_URL,
-                    data=dict(email=self._email, senha=self._password),
-                )
-                data = await response.json()
-                x_token = data.get("token")
-                response = await self.session.get(url, headers={"x-token": x_token})
-                self._supplies = await response.json()
-        except Exception as error:
-            _LOGGER.debug("%s - Could not update - %s", self._name, error)
-
     @property
     def name(self):
-        """Nome."""
+        """Return the name sensor"""
         return self._name
 
     @property
+    def icon(self):
+        """Return the default icon"""
+        return ICON
+
+    @property
     def state(self):
-        """Estado."""
-        return self._state
+        """Retorna o número de abastecimentos até então."""
+        return len(self._supplies)
 
     @property
     def supply(self):
@@ -111,16 +136,13 @@ class DrivvoSupplySensor(Entity):
 
     @property
     def cheapest_gasoline_until_today(self):
+        """Gasolina mais barata até hoje."""
         return min([supply.get("preco") for supply in self._supplies])
 
     @property
     def total_amount_of_supplies(self):
-        return sum([1 for supply in self._supplies])
-
-    @property
-    def icon(self):
-        """Icone."""
-        return "mdi:gas-station"
+        """Número total de abastetimentos"""
+        return len(self._supplies)
 
     @property
     def device_state_attributes(self):
@@ -128,16 +150,20 @@ class DrivvoSupplySensor(Entity):
         return {
             "veiculo": self._model,
             "odometro": self.supply.get("odometro"),
-            "posto_combustivel": self.supply.get("posto_combustivel").get("nome"),
-            "combustivel": self.supply.get("combustivel"),
-            "motivo": self.supply.get("tipo_motivo"),
-            "data": self.supply.get("data"),
-            "volume": self.supply.get("tanques")[0].get("volume"),
-            "valor": self.supply.get("valor_total"),
-            "preco": self.supply.get("preco"),
+            "posto": self.supply.get("posto_combustivel").get("nome"),
+            "tipo_de_combustivel": self.supply.get("combustivel"),
+            "motivo_do_abastecimento": self.supply.get("tipo_motivo"),
+            "data_do_abastecimento": self.supply.get("data"),
+            "volume_de_combustivel": self.supply.get("tanques")[0].get("volume"),
+            "valor_total_pago": self.supply.get("valor_total"),
+            "preco_do_combustivel": self.supply.get("preco"),
             "soma_total_de_abastecimentos": self.total_amount_of_supplies,
             "soma_total_de_valores_pagos_em_todos_os_abastecimentos": self.total_payment,
-            "tanque_cheio": "Sim" if self.supply.get("tanque_cheio") else "Não",
+            "encheu_o_tanque": "Sim" if self.supply.get("tanque_cheio") else "Não",
             "km_percorridos_desde_o_ultimo_abastecimento": self.km_travel,
             "gasolina_mais_barata_ate_entao": self.cheapest_gasoline_until_today,
         }
+
+    def update(self):
+        """Atualiza os dados fazendo requisição na API."""
+        self._supplies = get_data(self._email, self._password, self._id_vehicle)
