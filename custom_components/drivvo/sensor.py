a61031e10ddf3712ae35db5ad052280c8bd5f4c1
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2023-04-21 21:35:15 +0000

    feat: add config flow

--- custom_components/drivvo/sensor.py
+++ custom_components/drivvo/sensor.py
@@ -1,41 +1,31 @@
 import hashlib
-import json
 import logging
-import string
-from collections import defaultdict
-from datetime import datetime, timedelta
 
+from homeassistant import core, config_entries
 import homeassistant.helpers.config_validation as cv
-import pytz
 import requests
 import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
-    ATTR_ATTRIBUTION,
-    CONF_NAME,
-    CONF_RESOURCES,
     STATE_UNKNOWN,
 )
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from .const import (
+    DOMAIN,
+    CONF_EMAIL,
+    CONF_MODEL,
+    CONF_PASSWORD,
+    CONF_ID_VEHICLE,
+    SCAN_INTERVAL,
+    BASE_URL,
+    LOGIN_BASE_URL,
+    ICON,
+)
+
 from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
-
-ICON = "mdi:gas-station"
-
-SCAN_INTERVAL = timedelta(minutes=60)
-
-ATTRIBUTION = "Data provided by drivvo api"
-
-DOMAIN = "drivvo"
-
-CONF_EMAIL = "email"
-CONF_PASSWORD = "password"
-CONF_MODEL = "model"
-CONF_ID_VEHICLE = "id_vehicle"
-
-
-LOGIN_BASE_URL = "https://api.drivvo.com/autenticacao/login"
-BASE_URL = "https://api.drivvo.com/veiculo/{}/{}/web"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
@@ -70,22 +60,32 @@ def get_data(email, password, id_vehicle):
     return supplies
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Setup the currency sensor"""
-    name = "{} - Abastecimento".format(config["model"])
-    email = config["email"]
-    password = config["password"]
-    model = config["model"]
-    id_vehicle = config["id_vehicle"]
+async def async_setup_entry(
+    hass: core.HomeAssistant,
+    config_entry: config_entries.ConfigEntry,
+    async_add_entities,
+) -> None:
+    """Setup sensor platform."""
+    config = hass.data[DOMAIN][config_entry.entry_id]
 
-    add_entities(
-        [DrivvoSensor(hass, name, email, password, model, id_vehicle, SCAN_INTERVAL)],
-        True,
+    session = async_get_clientsession(hass)
+    async_add_entities(
+        [
+            DrivvoSensor(
+                hass,
+                config[CONF_EMAIL],
+                config[CONF_MODEL],
+                config[CONF_ID_VEHICLE],
+                config[CONF_PASSWORD],
+                SCAN_INTERVAL,
+            )
+        ],
+        update_before_add=True,
     )
 
 
 class DrivvoSensor(Entity):
-    def __init__(self, hass, name, email, password, model, id_vehicle, interval):
+    def __init__(self, hass, email, model, id_vehicle, password, interval):
         """Inizialize sensor"""
         self._state = STATE_UNKNOWN
         self._hass = hass
@@ -94,7 +94,7 @@ class DrivvoSensor(Entity):
         self._password = password
         self._model = model
         self._id_vehicle = id_vehicle
-        self._name = name
+        self._name = model
         self._supplies = []
 
     @property
